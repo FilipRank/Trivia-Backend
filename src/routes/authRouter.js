e1e291111466a2643e19a829d1af7db35b07a055
FilipRank <42890466+FilipRank@users.noreply.github.com>
2025-03-06 21:36:22 +0100

    change redirect for auth

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,14 +1,17 @@
 import express from 'express'
 import passport from 'passport'
+import dotenv from 'dotenv'
+
+dotenv.config
 
 const router = express.Router()
 
 router.get('/github', passport.authenticate('github', { scope: ['user.email'] }))
 
 router.get('/github/callback',
-  passport.authenticate('github', { failureRedirect: '/' }),
+  passport.authenticate('github', { failureRedirect: `${process.env.FRONTEND_URL}/login` }),
   (req, res) => {
-    res.redirect('/user')
+    res.redirect(`${process.env.FRONTEND_URL}`)
   }
 )
 
