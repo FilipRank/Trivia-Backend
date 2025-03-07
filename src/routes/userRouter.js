e411fd8f325f1be6eb51f213f12aa29ec3b0b0d9
FilipRank <42890466+FilipRank@users.noreply.github.com>
2025-03-07 11:37:39 +0100

    update router

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,13 +1,31 @@
 import express from 'express'
+import User from '../models/User.js'
 
 const router = express.Router()
 
+router.use((req, res, next) => {
+  if (!req.user) return res.status(401).send('Not authenticated')
+  next()
+})
+
 router.get('/', (req, res) => {
-  if (req.user) {
-    res.json({user: req.user})
+  res.json({user: req.user})
+})
+
+router.patch('/increase-balance', async (req, res) => {
+  try {
+    const amount = req.query.amount
+
+    if (!amount) return res.status(400).json({error: '"amount" parameter required'})
+    if (amount < 1) return res.status(400).json({error: 'amount cannot be less than 1'})
+    
+    await User.findByIdAndUpdate(req.user._id, {$inc: {balance: amount}})
+    const updatedUser = await User.findById(req.user._id)
+    res.status(200).json({user: updatedUser})
   }
-  else {
-    res.status(401).send('Not authenticated')
+  catch (err) {
+    res.status(500).json({error: err})
+    console.error(err)
   }
 })
 
