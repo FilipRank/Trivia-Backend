45071ba4888b57dfa8960401e65a3e72ca071b02
FilipRank <42890466+FilipRank@users.noreply.github.com>
2025-05-18 15:17:37 +0200

    fix hard-coded image url

--- src/routes/badgeRouter.js
+++ src/routes/badgeRouter.js
@@ -1,10 +1,16 @@
 import express from 'express'
 import Badge from '../models/Badge.js'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const router = express.Router()
 
 router.get('/', async (req, res) => {
   const badges = await Badge.find()
+  if (process.env.BACKEND_URL) {
+    badges.forEach(badge => {badge.imageUri = process.env.BACKEND_URL + `/badges/${badge.name.toLowerCase().replace(/\s+/g, '')}.svg`})
+  }
   res.json(badges)
 })
 
@@ -12,6 +18,8 @@ router.get('/:id', async (req, res) => {
   try {
     const badge = await Badge.findOne({_id: req.params.id})
     if (!badge) res.status(404).json({msg: `No badge found with id ${req.params.id}`})
+    if (process.env.BACKEND_URL) 
+      badge.imageUri = process.env.BACKEND_URL + `/badges/${badge.name.toLowerCase().replace(/\s+/g, '')}.svg`
     res.status(200).json(badge)
   }
   catch (err) {
